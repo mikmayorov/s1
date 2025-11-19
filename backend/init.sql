98a2664048794a92ebe4ad404c64c4beb8503826
Mikhail Mayorov <mm@pg19.ru>
2025-11-20 00:13:12 +0300

    легкий лоск

--- backend/init.sql
+++ backend/init.sql
@@ -1,3 +1,3 @@
-create role s1api login encrypted password 'Xie0mahp';
+create role s1api login encrypted password 'password';
 GRANT CONNECT ON DATABASE b TO s1api;
 GRANT USAGE ON SCHEMA public TO s1api;
